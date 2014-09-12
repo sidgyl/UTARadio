faaace2f41ff7d728ae7cbd629a84c9eaeee26b5
Ehidiamen Ojielu <Ehis@Ehidiamens-MacBook-Pro.local>
2014-09-12 18:51:00 -0500

    Initial Commit

---
+++ UTARadio/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UTARadio
+//
+//  Created by Ehidiamen Ojielu on 9/12/14.
+//  Copyright (c) 2014 mobi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UTARadio/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UTARadio
+//
+//  Created by Ehidiamen Ojielu on 9/12/14.
+//  Copyright (c) 2014 mobi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
